278a227f541a3863988d2916857a516092fb7ffc
Abdulraheem Ahmad <abdulahmad586@gmail.com>
2024-09-01 11:51:11 +0100

    First commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const port = 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: "Server is alive at " + new Date() });
+});
+
+app.post('/', (req, res) => {
+    const { status } = req.body;
+    console.log("Status :", status, "At:", new Date());
+
+    res.status(200).json({ msg: "Status recorded as " + status });
+});
+
+app.listen(port, () => {
+    console.log(`Intruder Alert Server running on ${port}`);
+});
